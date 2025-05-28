6c309acbc5251b65a5fe4fc5e900170838d49d57
nindy <nindya.ayu5p@gmail.com>
2025-05-28 09:20:34 +0700

    model Barang untuk data stok

---
+++ lib/models/barang.dart
@@ -0,0 +1,38 @@
+class Barang {
+  final int id;
+  final String nama;
+  final int stok;
+  final String deskripsi;
+
+  Barang({
+    required this.id,
+    required this.nama,
+    required this.stok,
+    required this.deskripsi,
+  });
+
+  factory Barang.fromJson(Map<String, dynamic> json) {
+    return Barang(
+      id: json['id'],
+      nama: json['nama'],
+      stok: json['stok'],
+      deskripsi: json['deskripsi'],
+    );
+  }
+
+  Map<String, dynamic> toJson() => {
+    'id': id,
+    'nama': nama,
+    'stok': stok,
+    'deskripsi': deskripsi,
+  };
+
+  Barang copyWith({int? id, String? nama, int? stok, String? deskripsi}) {
+    return Barang(
+      id: id ?? this.id,
+      nama: nama ?? this.nama,
+      stok: stok ?? this.stok,
+      deskripsi: deskripsi ?? this.deskripsi,
+    );
+  }
+}
